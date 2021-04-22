e2e4690536c49ac1bf65b3ad2d3a7f719afd7c21
Universidad Politécnica de Tlaxcala <condeguillermo8@gmail.com>
2021-04-22 15:23:31 -0500

    Reinicio de app

--- application/views/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-    <strong>Copyright &copy; 2014-2020 <a href="http://uptlax.edu.mx/">Universidad Politécnica de Tlaxcala</a>.</strong>
-    Todos los derechos reservados.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.1.0-rc
-    </div>
-  </footer>
